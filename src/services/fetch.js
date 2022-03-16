332ce1931f43c9d71f6e9aca81cf2fa6cb791dcd
Andrea Cleland <andreaclelan@gmail.com>
2022-03-16 12:45:13 -0700

    implemented fetchPokemon and displayed list of pokemon names to Main

---
+++ src/services/fetch.js
@@ -0,0 +1,5 @@
+export async function fetchPokemon() {
+  const resp = await fetch('https://pokedex-alchemy.herokuapp.com/api/pokedex');
+  const data = await resp.json();
+  return data.results;
+}
--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { fetchPokemon } from '../../services/fetch';
 
 export default function Main() {
-  return <div>Main</div>;
+  const [pokeList, setPokeList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const pokeData = await fetchPokemon();
+      setPokeList(pokeData);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      Main
+      <ul>
+        {pokeList.map((item) => (
+          <li key={item.id}>{item.pokemon}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
