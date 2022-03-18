8d8c11e065d87245ab4b31842cba979886ade613
Andrea Cleland <andreaclelan@gmail.com>
2022-03-18 12:37:51 -0700

    added placeholder text for search field

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,7 +4,13 @@ export default function Search({ searchText, handleChange, handleSubmit }) {
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="search"></label>
-      <input type="text" name="search" value={searchText} onChange={handleChange} />
+      <input
+        type="text"
+        name="search"
+        placeholder="Search Pokemon by Name"
+        value={searchText}
+        onChange={handleChange}
+      />
       <button>Search</button>
     </form>
   );
